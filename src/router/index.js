44794ecce678d860aa4c25d0070dc7739b33256a
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-07-28 18:36:00 +0100

    Create login and signup

--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('@/views/About.vue'),
   },
   {
     path: '/contacts',
     name: 'Contacts',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '@/components/Contacts'),
+    component: () => import('@/components/Contacts'),
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/views/Login.vue'),
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () => import('@/views/SignUp.vue'),
   },
 ];
 
---
+++ src/views/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label for="username">Username</label>
+        <input
+          v-model="userName"
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input v-model="email" type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input
+          v-model="password"
+          type="password"
+          name="password"
+          placeholder="Passowrd"
+        />
+      </div>
+      <div>
+        <label for="password_confirmation">Password Confirmation</label>
+        <input
+          v-model="password_confirmaion"
+          type="password"
+          name="password_confirmation"
+          placeholder="Passowrd Confirmation"
+        />
+      </div>
+      <div>
+        <button type="submit">Sign Up</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      form: {
+        userName: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {},
+  },
+};
+</script>
