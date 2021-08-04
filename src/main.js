794272f8da4a477b1c06947243339ff80f95da2e
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-08-04 19:59:13 +0100

    Import the api service instances

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { securedAxiosInstance, plainAxiosInstance } from './services/index';
 import '@/assets/css/tailwind.css';
 
 Vue.config.productionTip = false;
 
+Vue.use({
+  secure: securedAxiosInstance,
+  plain: plainAxiosInstance,
+});
+
 new Vue({
   router,
+  securedAxiosInstance,
+  plainAxiosInstance,
   render: (h) => h(App),
 }).$mount('#app');
