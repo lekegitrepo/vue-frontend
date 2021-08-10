9eca513b364bc3c05dbe994423ff14ae5815beef
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-08-10 19:15:12 +0100

    Add tabs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app" class="flex flex-col items-center">
+    <Navbar
+      allowUnselect
+      :tabs="[
+        { name: 'Features', value: 'feautures' },
+        { name: 'Delivery', value: 'delivery' },
+        { name: 'Shipping', value: 'shipping' },
+        { name: 'About', value: 'about' },
+      ]"
+    />
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>|
@@ -9,3 +18,13 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar';
+//import Navbar from './components/Navbar.vue';
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
