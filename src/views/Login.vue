94b4d4a108432cd4d9d651a9c9ad1bff55c60a14
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-08-14 17:59:09 +0100

    Add login contents

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label for="email">Email</label>
+        <input v-model="email" type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input
+          v-model="password"
+          type="password"
+          name="password"
+          placeholder="Passowrd"
+        />
+      </div>
+      <div>
+        <button type="submit">Sign Up</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {},
+  },
+};
+</script>
