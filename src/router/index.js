66953143fb23bf7ad17581d359827b6e9f353b73
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-07-12 07:28:54 +0100

    Update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,41 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: "/",
+    name: "login",
+    component: () => import("@/views/login.vue"),
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import("@/views/dashboard"),
+  },
+  {
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('@/views/About.vue'),
+    component: () => import("@/views/about"),
   },
   {
-    path: '/contacts',
-    name: 'Contacts',
-    component: () => import('@/components/Contacts'),
+    path: "/contacts",
+    name: "contacts",
+    component: () => import("@/components/contacts"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('@/views/Login.vue'),
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: () => import('@/views/SignUp.vue'),
+    path: "/signup",
+    name: "signup",
+    component: () => import("@/views/sign-up"),
   },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
