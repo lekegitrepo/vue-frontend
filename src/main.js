b4032381ec09ccf9f22015800fc139697861c7a3
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-07-22 09:09:37 +0100

    Add vuelidate

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue';
+import Vue from "vue";
 //import VueResource from 'vue-resource';
-import VueAxios from 'vue-axios';
-import App from './App.vue';
-import router from './router';
+import VueAxios from "vue-axios";
+import Vuelidate from "vuelidate";
+import App from "./App.vue";
+import router from "./router";
 //import axios from 'axios';
-import { securedAxiosInstance, plainAxiosInstance } from './services/index';
-import Multiselect from 'vue-multiselect';
-import '@/assets/css/tailwind.css';
+import { securedAxiosInstance, plainAxiosInstance } from "./services/index";
+import Multiselect from "vue-multiselect";
+import "@/assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
 
@@ -14,14 +15,15 @@ Vue.use(VueAxios, {
   secure: securedAxiosInstance,
   plain: plainAxiosInstance,
 });
+Vue.use(Vuelidate);
 //Vue.use(VueResource);
 //Vue.prototype.$http = axios;
 // register globally
-Vue.component('multiselect', Multiselect);
+Vue.component("multiselect", Multiselect);
 
 new Vue({
   router,
   securedAxiosInstance,
   plainAxiosInstance,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
